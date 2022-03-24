46ae4536d9fc756fc4df1e7c4fe3a177131153bb
Adriel Moreno <adrielmoreno@hotmail.com>
2022-03-25 00:04:56 +0100

    Ejemplos de Tuplas y Enums

---
+++ IntroSwift.playground/Pages/U03_Tuplas y enums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+/*** Tuplas ***/
+let mailbox = ("Adriel", 35)
+mailbox.0
+mailbox.1
+
+// Tupla asignandole alias
+let mailboxAlias: (name: String, number: Int) = ("Adriel", 35)
+mailboxAlias.name
+mailboxAlias.number
+
+
+
+/*** Enum ***/
+// enum báscio. CaseIterable permite que se pueda iterar en el enum
+enum LetterType: CaseIterable {
+    case love
+    case business
+    case family
+    case friends
+}
+
+var letterType: LetterType = .business
+// otra forma de declarar accediendo al enum
+var letterType2 = LetterType.family
+
+
+// accediendo a todos los casos. El enum debe conformar el protocolo CaseIterable
+LetterType.allCases
+
+//asociando valor a los casos
+enum MailboxProperties {
+    case capacity(Int)
+    case address(String)
+}
+
+var mailboxProperty = MailboxProperties.address("Valladolid")
+mailboxProperty = MailboxProperties.capacity(500)
+
+// Enum definiendo el tipo
+enum LetterSizes: Int {
+    case small = 5 // se autincrementan los demás valores
+    case medium
+    case big
+}
+
+var letterSize = LetterSizes(rawValue: 7) // big
