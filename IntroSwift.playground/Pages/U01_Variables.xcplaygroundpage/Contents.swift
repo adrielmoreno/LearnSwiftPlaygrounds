ee70d2600ac42c6c27d82b0260d94ef4e6166f30
Adriel Moreno <adrielmoreno@hotmail.com>
2022-03-24 23:31:04 +0100

    Ejemplos de variables

--- IntroSwift.playground/Pages/U01_Variables.xcplaygroundpage/Contents.swift
+++ IntroSwift.playground/Pages/U01_Variables.xcplaygroundpage/Contents.swift
@@ -1,1 +1,34 @@
+/*** VARIABLES Y TIPOS ***/
 
+// Definiendo una variable
+var letter: String
+letter = "Estoy practicando con Swift"
+
+// Definiendo una constante
+let letterApril: String
+letterApril = "Me encanta el curso de Swift"
+
+
+
+// String, Int, Float, Bool
+var numberOfLetters: UInt = 10
+// numberOfLetters = -10 //error UInt es sólo para enteros
+
+var age: Int = 35
+age = -1
+
+var letterPrice: Float = 1.29
+
+var letterBigPrice: Double = 1.2929292992929
+
+var isLetterSended: Bool = true
+isLetterSended = false
+
+
+// Casting
+var ageSum = age + Int(numberOfLetters)
+
+// Operadores básicas: Multiplicación *, División /, Suma +, Resta -, Módulo %
+
+var modulo = 10 % 3
+var division = 10 / 3
