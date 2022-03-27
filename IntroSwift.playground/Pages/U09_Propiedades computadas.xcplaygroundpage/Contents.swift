9e694f3a6773edf4850e88a680aa55fc1c307c43
MorenoCreativo <adrielmoreno@hotmail.com>
2022-03-27 13:23:18 +0200

    Prop. Computadas con willset y didSet

--- IntroSwift.playground/Pages/U09_Propiedades computadas.xcplaygroundpage/Contents.swift
+++ IntroSwift.playground/Pages/U09_Propiedades computadas.xcplaygroundpage/Contents.swift
@@ -21,3 +21,24 @@ coffeShop.coffeesPerClient
 coffeShop.coffeesPerClient = 10
 coffeShop.coffees
 
+// Observadores
+struct CoffeShopObservers {
+    var price: Float = 0.5
+    var coffees: Int {
+        //cuando se va a modificar la variable
+        willSet(newTotalCoffees) {
+            print("Viene más café \(newTotalCoffees)")
+        }
+        //después de modificar la variable
+        didSet {
+            print("Tenemos más café \(coffees)")
+            totalCoffeesPrice = Float(coffees) * price
+        }
+    }
+    
+    var totalCoffeesPrice: Float = 0
+}
+
+var coffee = CoffeShopObservers(coffees: 10)
+coffee.coffees = 15
+coffee.totalCoffeesPrice
